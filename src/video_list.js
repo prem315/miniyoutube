2eb13cad12167102f7993b065d89719f2c76e6f1
Prem Patel <prem.patel3105@gmail.com>
2017-07-17 19:07:10 +0530

    added video_list_item and display

--- src/video_list.js
+++ src/video_list.js
@@ -1,26 +1,17 @@
 import React, {Component} from 'react';
+import VideoListItem from './video_list_item';
 
-// const VideoList = (props) => {
-//   console.log(props);
-//     return(
-//       <ul className="col-md-4 list-group">
-//
-//       </ul>
-//     )
-// }
-
-class VideoList extends Component {
-  constructor(props){
-    super(props);
-    console.log(props);
-  }
-  render(){
+const VideoList = (props) => {
+  console.log(props);
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video}></VideoListItem>
+  });
     return(
       <ul className="col-md-4 list-group">
-        {this.props.videos.length}
+        {videoItems}
+
       </ul>
     )
-  }
 }
 
 export default VideoList;
